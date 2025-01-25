aaa46e916db58537716d89b429c70adf68d639eb
GmMamunH <gmmamun3004@gmail.com>
2025-01-25 16:50:35 +0600

    first commit

---
+++ src/components/video-description/VideoPlayer.jsx
@@ -0,0 +1,13 @@
+/* eslint-disable react/prop-types */
+export default function VideoPlayer({videoTitle, videoLink}){
+    return (
+        <iframe
+            width="100%"
+            className="aspect-video"
+            src={videoLink}
+            title={videoTitle}
+            allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+            allowfullscreen
+        ></iframe>
+    )
+}
