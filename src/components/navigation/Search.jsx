bc8dc923ba2aad0fac8542bfb77fdf3263b4135f
GmMamunH <gmmamun3004@gmail.com>
2025-01-25 19:20:21 +0600

    add(search): add debounce in search

--- src/components/navigation/Search.jsx
+++ src/components/navigation/Search.jsx
@@ -1,24 +1,35 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import { useDispatch } from "react-redux";
 import { setSearchTerm } from "../../features/filter/filterSlice";
+import { useNavigate } from "react-router-dom";
 
-export default function Search(){
-    const [input, setInput] = useState('');
-    const dispatch = useDispatch();
-    const handleSubmit = (e) => {
-        e.preventDefault();
-        dispatch(setSearchTerm(input))
-    }
-    return (
-        <form onSubmit={handleSubmit} >
-            <input
-                className="outline-none border-none mr-2"
-                type="search"
-                name="search"
-                placeholder="Search"
-                value={input}
-                onChange={(e)=>setInput(e.target.value)}
-            />
-        </form>
-    )
+export default function Search() {
+  const navigate = useNavigate();
+  const [searchDebounce, setSearchDebounce] = useState("");
+  const [input, setInput] = useState("");
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      const trimmedInput = input.trim();
+      setSearchDebounce(trimmedInput);
+      dispatch(setSearchTerm(trimmedInput));
+      navigate("/");
+    }, 1000);
+
+    return () => clearTimeout(timer);
+  }, [input, dispatch, navigate]);
+
+  return (
+    <form>
+      <input
+        className="outline-none border-none mr-2"
+        type="search"
+        name="search"
+        placeholder="Search"
+        value={input}
+        onChange={(e) => setInput(e.target.value)}
+      />
+    </form>
+  );
 }
